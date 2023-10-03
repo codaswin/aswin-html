c2fd83df878a347a3c251a02ceae45974113978f
aswin <sekaraswin00@gmail.com>
2023-10-03 07:56:50 +0530

    set radio button unique

--- javascript-amazon-project/scripts/checkout.js
+++ javascript-amazon-project/scripts/checkout.js
@@ -49,7 +49,7 @@ cart.forEach((cartItem) => {
 
         <div class="delivery-option">
           <input type="radio" class="delivery-option-input"
-            name="delivery-option-2">
+            name="delivery-option-${matchingProduct.id}">
           <div>
             <div class="delivery-option-date">
               Tuesday, June 21
@@ -61,7 +61,7 @@ cart.forEach((cartItem) => {
         </div>
         <div class="delivery-option">
           <input type="radio" checked class="delivery-option-input"
-            name="delivery-option-2">
+            name="delivery-option-${matchingProduct.id}">
           <div>
             <div class="delivery-option-date">
               Wednesday, June 15
@@ -73,7 +73,7 @@ cart.forEach((cartItem) => {
         </div>
         <div class="delivery-option">
           <input type="radio" class="delivery-option-input"
-            name="delivery-option-2">
+            name="delivery-option-${matchingProduct.id}">
           <div>
             <div class="delivery-option-date">
               Monday, June 13
